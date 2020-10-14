fe3c97102d20f74fa833dbbc5eb7c27e8d3414f5
Aliah siddiqah <aliahsiddiqah@gmail.com>
2020-10-14 18:12:25 +0800

    First commit week 1

---
+++ Driver.java
@@ -0,0 +1,22 @@
+//Author: aliah siddiqah
+
+public class Driver{
+	
+	//Attribute or data meber of fields
+	public static void main (String [] org) {
+		//Fish actually is a data type
+		//Fish nemo; == int x; whatever lepas perkataan new panggil balik class tu and kurungan
+		Fish nemo = new Fish(); //nemo adalah objecr, Fish adalah kelas
+		nemo.Swim(); //kena memalui object baru boleh
+		nemo.color = "red"; //tukar value
+		nemo.Swim();
+
+		Fish dory = new Fish(); //another object
+		dory.Swim();
+
+
+	}
+
+	
+
+	}
---
+++ Fish.java
@@ -0,0 +1,12 @@
+//Author: aliah siddiqah
+public class Fish {
+	
+	//Attribute or data meber of fields
+	public String color = "blue"; //default value
+
+	//method
+	public void Swim () {
+		System.out.println(color + " fish swim");	}
+
+
+	}
